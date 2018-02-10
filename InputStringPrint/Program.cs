1879d430fd25751976ba603c2cf0d38cc3cf3d31
Bishnu Gopali <bishnugopali@hotmail.com>
2018-02-10 12:05:08 +0000

    Enter and print string

--- InputStringPrint/Program.cs
+++ InputStringPrint/Program.cs
@@ -10,6 +10,17 @@ namespace InputStringPrint
     {
         static void Main(string[] args)
         {
+            var str1="";
+            Console.WriteLine("Please Enter Something");
+            str1 = Convert.ToString(Console.ReadLine());
+
+            Console.WriteLine("You Entered following:");
+            Console.Write("{0}",str1);
+
+            Console.ReadLine();
+
+
+
         }
     }
 }
